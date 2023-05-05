fe223c9d47e853c92b78655a028597a8e52ef604
se3daa <116446045+se3daa@users.noreply.github.com>
2023-05-06 02:04:41 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/Class.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, doctor!");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Created("", value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
